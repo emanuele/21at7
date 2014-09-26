7924a4cb7ea955b6c56c50de4cd3052009090bf7
Emanuele Olivetti <olivetti@fbk.eu>
2014-09-27 00:15:07 +0200

    added sump on db of heating decision + mini demo of that

--- heating.py
+++ heating.py
@@ -6,13 +6,14 @@ class HeatingStandardSchedule(object):
     """This is the standard (naive) heating schedule most of people
     have at home.
     """
-    def __init__(self, T_min=20, T_max=22, off_hours=(23,4), off_months=(6,8), T_warning=10.0):
+    def __init__(self, T_min=20, T_max=22, off_hours=(23,4), off_months=(6,8), T_warning=10.0, engine=None):
         self.T_min = T_min
         self.T_max = T_max
         self.off_hours = off_hours
         self.off_months = off_months
         self.T_warning = T_warning
         self.heating = 0.0
+        self.engine = engine
 
         
     def heating_action(self, my_datetime, my_temperature):
@@ -31,6 +32,12 @@ class HeatingStandardSchedule(object):
             else: # Otherwise, keep previous schedule:
                 self.heating = self.heating_previous
 
+        if self.engine is not None:
+            df = pd.DataFrame({'timestamp': [my_datetime],
+                               'heating': [self.heating],
+                               })
+            df.to_sql('heating', self.engine, if_exists='append')
+
         return self.heating
         
 
@@ -43,11 +50,19 @@ if __name__ == '__main__':
     from sqlalchemy import create_engine
 
     engine = create_engine('sqlite:///21at7.sqlite')
-    dataset_external_temperature = pd.read_sql_table('temperature_external', engine)
+    dataset_external_temperature = pd.read_sql_table('temperature_external', engine)[:100]
     timestamps = dataset_external_temperature['timestamp'].values
     external_temperature = dataset_external_temperature['external_temperature'].values
-    hss = HeatingStandardSchedule()
-    heating = np.array([hss.heating_action(ts, et) for i, et, ts in dataset_external_temperature.values])
+    try:
+        pd.io.sql.execute("drop table heating", con=engine)
+    except OperationalError: # in case table 'heating' does not exist
+        pass
+    
+    hss = HeatingStandardSchedule(engine=engine)
+    heating = np.zeros(dataset_external_temperature.shape[0])
+    for idx, (i, et, ts) in enumerate(dataset_external_temperature.values):
+        if (idx % 20) == 0: print(idx)
+        heating[idx] = hss.heating_action(ts, et)
     
     plot = True
     if plot:
