6ed296c5111d7bd3d2fb82e9803324028ab30399
Emanuele Olivetti <emanuele.olivetti@gmail.com>
2014-09-28 14:57:28 +0200

    refactored constants and parameters + improved printouts

--- demo_simulate_home.py
+++ demo_simulate_home.py
@@ -9,32 +9,33 @@ from sqlalchemy.exc import OperationalError
 
 if __name__ == '__main__':
 
-    # Constants for heat transfer:
-    k_home_external = 1.0 / 3600.0 / 2.0 # how fast home temperature changes due to external temperature
-    k_home_heater = 1.0 / 3600.0 # how fast home temperature changes due to heater temperature
-    k_heater_on = 1.0 / 600.0 # how fast heater temperature changes when heating is on
-    k_heater_off = 1.0 / 1200.0 # how fast heater temperature changes when heating is off
+    from configuration import *
 
-    T_heating = 50.0 # temperature of the heating system, i.e. of the hot water produced
-    T0_home = 20.0 # initial house temperature # initial house temperature at time 0.
-
-    engine = create_engine('sqlite:///21at7.sqlite')
-    dataset_external_temperature = pd.read_sql_table('temperature_external', engine)[:100]
+    print("21at7: simulation of home temperature given external temperature and heating schedule.")
+    engine = create_engine(engine_string)
+    print("Loading external temperatures from %s" % engine)
+    limit = 100
+    print("Limiting the records to the first %d." % limit)
+    dataset_external_temperature = pd.read_sql_table('temperature_external', engine)[:limit]
     timestamps = dataset_external_temperature['timestamp'].values
-    t_step = float(np.diff(timestamps).mean()) / 1.0e9
+    t_step = float(np.diff(timestamps).mean()) / 1.0e9 # differences between timestamps are originally in nanoseconds, but we want seconds
     external_temperature = dataset_external_temperature['external_temperature'].values
+
     try:
+        print("Dropping previously existing table temperature_external.")
         pd.io.sql.execute("drop table temperature_home", con=engine)
     except OperationalError:
         print("Table 'temperature_home' does not exist.")
         pass
     
     try:
+        print("Dropping previously existing table heating.")
         pd.io.sql.execute("drop table heating", con=engine)
     except OperationalError:
         print("Table 'heating' does not exist.")
         pass
-    
+
+    print("Starting the simulation.")
     hss = HeatingStandardSchedule(engine=engine)
     ht = HomeTemperature(T_heating, k_home_external, k_heater_on, k_heater_off, k_home_heater, t_step, engine=engine)
     heating = np.zeros(dataset_external_temperature.shape[0])
@@ -43,10 +44,11 @@ if __name__ == '__main__':
     T_home[0] = T0_home
     T_heater[0] = T0_home
     for idx, (i, ex, ts) in enumerate(dataset_external_temperature.values[:-1]):
-        if (idx % 20) == 0: print(idx)
+        if (idx % 20) == 0: print(ts)
         heating[idx] = hss.heating_action(ts, T_home[idx])
         T_home[idx + 1], T_heater[idx + 1] = ht.home_heater_temperature(T_home[idx], ex, T_heater[idx], heating[idx], ts)
 
+    print("End of the simulation.")
     
     plot = True
     if plot:
