2332099c9719f94586e0d66bcaf6ce5a03c51925
Emanuele Olivetti <olivetti@fbk.eu>
2014-10-01 22:54:32 +0200

    enhanced demo now including preliminary example of HeatingOptimizedSchedule in action

--- demo_simulate_home.py
+++ demo_simulate_home.py
@@ -5,7 +5,7 @@ from simulate_external import simulate_external_temperature
 from simulate_home import HomeTemperature
 from sqlalchemy import create_engine
 from sqlalchemy.exc import OperationalError
-from configuration import time_step
+from configuration import time_step, T_warning
 
 
 if __name__ == '__main__':
@@ -38,17 +38,30 @@ if __name__ == '__main__':
 
     print("Starting the simulation.")
     hss = HeatingStandardSchedule(engine=engine)
+    hos = HeatingOptimizedSchedule(time_step=time_step, T_warning=T_warning, engine=engine)
     ht = HomeTemperature(T_heating, k_home_external, k_heater_on, k_heater_off, k_home_heater, t_step, engine=engine)
     heating = np.zeros(dataset_external_temperature.shape[0])
     T_home = np.zeros(dataset_external_temperature.shape[0])
     T_heater = np.zeros(dataset_external_temperature.shape[0])
     T_home[0] = T0_home
     T_heater[0] = T0_home
-    for idx, (ex, ts) in enumerate(dataset_external_temperature.values[:-1]):
+    switch_at = 150
+    print("Starting with HeatingStandardSchedule:")
+    for idx, (ex, ts) in enumerate(dataset_external_temperature.values[:-1][:switch_at]):
         if (idx % 20) == 0: print("%d) %s" % (idx, ts))
         ts = np.datetime64(ts).astype(object) # this transform string into datetime64 first and datetime.datetime then.
         heating[idx] = hss.heating_action(ts, T_home[idx])
+        # heating[idx] = hos.heating_action(ts)
         T_home[idx + 1], T_heater[idx + 1] = ht.home_heater_temperature(T_home[idx], ex, T_heater[idx], heating[idx], ts)
+
+    print("Switching to HeatingOptimizedSchedule:")
+    for idx, (ex, ts) in enumerate(dataset_external_temperature.values[:-1][switch_at:]):
+        idx = idx + switch_at
+        if (idx % 20) == 0: print("%d) %s" % (idx, ts))
+        ts = np.datetime64(ts)
+        heating[idx] = hos.heating_action(ts)
+        T_home[idx + 1], T_heater[idx + 1] = ht.home_heater_temperature(T_home[idx], ex, T_heater[idx], heating[idx], ts)
+
 
     print("End of the simulation.")
     
