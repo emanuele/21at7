c1d64abc5657a1d531913d74a7e47579bb7fcd30
Enrico Marchesin <github.com@palio.net>
2015-01-14 19:04:36 +0100

    moving procedures related to harvested data cleaning into separate deamon

--- cleaner.py
+++ cleaner.py
@@ -1,4 +1,4 @@
-import glob,sys,time,threading,getopt,os
+import glob,sys,time,threading,getopt,os,tempfile
 
 from config import mainDB,lacrosse_serial,ciseco_serial,logdir
 from models import SessionMaker,Sensor
@@ -6,31 +6,35 @@ from harvesters import *
 from daemon import Daemon
 
 
-class Harvester(Daemon):
+class Cleaner(Daemon):
 	def run(self):
 		if lacrosse_serial:
-			self.lacrosse_reader=lacrosse.Reader(mainDB,lacrosse_serial,debug=True,dbdebug=False)
-			self.lacrosse_reader.start()
+			self.lacrosse_cleaner=lacrosse.Cleaner(mainDB,debug=True,dbdebug=False)
+			self.lacrosse_cleaner.start()
 		else:
 			sys.stderr.write('no lacrosse sensors')
+			sys.stderr.flush()
 
 		if ciseco_serial:
-			self.ciseco_reader=ciseco.Reader(mainDB,ciseco_serial,debug=True,dbdebug=False)
-			self.ciseco_reader.start()
+			self.ciseco_cleaner=ciseco.Cleaner(mainDB,debug=True,dbdebug=False)
+			self.ciseco_cleaner.start()
 		else:
 			sys.stderr.write('no ciseco sensors\n')
+			sys.stderr.flush()
 
-		sys.stderr.write('HARVESTING STARTED\n')
+		sys.stderr.write('CLEANING STARTED\n')
+		sys.stderr.flush()
 		while True:
 			time.sleep(1)
-		sys.stderr.write('HARVESTING ENDED\n')
+		sys.stderr.write('CLEANING ENDED\n')
+		sys.stderr.flush()
 
 
 if __name__ == '__main__':
 	if lacrosse_serial or ciseco_serial:
-		pid=os.path.join(os.getcwd(),'21at7_harvester.pid')
-		logfile=os.path.join(logdir,'21at7_harvester.log')
-		daemon=Harvester(pid,stdout=logfile,stderr=logfile)
+		pid=os.path.join(os.getcwd(),'21at7_cleaner.pid')
+		logfile=os.path.join(logdir,'21at7_cleaner.log')
+		daemon=Cleaner(pid,stdout=logfile,stderr=logfile)
 		if len(sys.argv) == 2:
 			if 'start' == sys.argv[1]:
 				daemon.start()
