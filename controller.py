b12c6fa4059eb75a75f6a8b43397356bdec1734a
Enrico Marchesin <github.com@palio.net>
2015-01-15 01:35:28 +0100

    first nearly complete controller implementation that checks measured temperature against desired temperature (zone by zone)

--- controller.py
+++ controller.py
@@ -1,13 +1,15 @@
-import glob,sys,time,threading,getopt,os,tempfile
+import glob,sys,time,threading,getopt,os,tempfile,traceback
 
 from config import mainDB,lacrosse_serial,ciseco_serial,logdir
-from models import SessionMaker,Sensor
+from models import SessionMaker,Sensor,Zone,Measure
 from harvesters import *
+from scheduler import Scheduler
+
 from daemon import Daemon
 
 
 def log(msg):
-	sys.stderr.write('[CLEANER] %s %s\n'%(time.strftime("%Y-%m-%d %H:%M:%S"), msg))
+	sys.stderr.write('[CONTROLLER] %s %s\n'%(time.strftime("%Y-%m-%d %H:%M:%S"), msg))
 
 
 class Controller(Daemon):
@@ -24,9 +26,48 @@ class Controller(Daemon):
 		else:
 			log('no ciseco sensors')
 
+		self.scheduler=Scheduler(mainDB,debug=False)
+		self.session_maker=SessionMaker(mainDB,debug=False)
+
 		log('CONTROLLER STARTED')
 		while True:
-			time.sleep(1)
+			
+			zones=None
+			try:
+				session = self.session_maker.get_session()
+				zones=session.query(Zone)
+				for zone in zones.all():
+					log('zone id: %d'%(zone.id))
+					desired=self.scheduler.desired(zone_id=zone.id)
+					log('desired temp in "%s": %.1f'%(zone.desc,desired))
+					min=None
+					for sensor in zone.sensors:
+						log('sensor: %s'%sensor.desc)
+						for measure in sensor.measurements:
+							if measure.what=='temp':
+								log('%s: %.1f'%(measure.what,measure.howmuch))
+								if min==None or measure.howmuch<min:
+									min=measure.howmuch
+					if min: 
+						log('min: %.1f'%min)
+						if min<(desired-0.5):
+							log('heating ON in "%s"'%zone.desc)
+						elif min>(desired+0.5):
+							log('heating OFF in "%s"'%zone.desc)
+					#latest=zone.sensors.measurements.order_by(Measure.when.desc(),Measure.howmuch).first()
+					#print 'latest measurement:',latest
+			except:
+				e = sys.exc_info()[0]
+				log('zones query error: %s'%e)
+				traceback.print_exc()
+			finally:
+				try:
+					session.close()
+				except:
+					zones=None
+					log('db (zones) session close error')
+
+			time.sleep(5)
 		log('CONTROLLER ENDED')
 
 
